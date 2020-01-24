10919ce525c12bf036dd128b9227cbecc1a93f47
Leandro Oliveira <oliveira.leandro826@gmail.com>
2020-01-24 17:01:18 -0300

    Atualização

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Especie, Imagem, Acesso
 
-# Register your models here.
+admin.site.register(Especie)
+admin.site.register(Imagem)
+admin.site.register(Acesso)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Especie(models.Model):
+    nome = models.CharField(max_length=256, verbose_name='Nome', blank=False, null=False)
+
+class Imagem(models.Model):
+    caminho = models.FileField(verbose_name='caminho', blank=False, null=False)
+
+class Acesso(models.Model):
+    nome = models.CharField(max_length=256, verbose_name='Nome', blank=False, null=False)
+    especie = models.OneToOneField(Especie, on_delete='cascade')
+    imagem = models.ManyToManyField(Imagem)
+
+
+
